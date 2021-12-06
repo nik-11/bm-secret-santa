45a75b321554695aca2308a829288c075b96118e
nik-11 <hello@nikolauswust.com>
2021-12-06 21:29:03 +0800

    Initial commit

---
+++ .history/infrastructure/lib/secret-santa-stack_20211206210418.ts
@@ -0,0 +1,16 @@
+import { Stack, StackProps } from 'aws-cdk-lib';
+import { LambdaRestApi } from 'aws-cdk-lib/aws-apigateway';
+import { IFunction } from 'aws-cdk-lib/aws-lambda';
+import { Construct } from 'constructs';
+
+declare const backend: IFunction;
+export class SecretSantaStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    new LambdaRestApi(this, 'SecretSantaApi', {
+      handler: backend,
+      proxy: false,
+    });
+  }
+}
---
+++ .history/infrastructure/lib/secret-santa-stack_20211206211537.ts
@@ -0,0 +1,34 @@
+import { Stack, StackProps } from 'aws-cdk-lib';
+import { LambdaRestApi, TokenAuthorizer } from 'aws-cdk-lib/aws-apigateway';
+import { AssetCode, IFunction } from 'aws-cdk-lib/aws-lambda';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import { Construct } from 'constructs';
+
+declare const backend: IFunction;
+export class SecretSantaStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    const api = new LambdaRestApi(this, 'SecretSantaApi', {
+      handler: backend,
+      proxy: false,
+    });
+
+    const authLambda = new NodejsFunction(this, 'AuthLambda', {
+      handler: 'index.handler',
+      code: AssetCode.fromAsset(path.join('')),
+    });
+    const auth = new TokenAuthorizer(this, 'SecretSantaAuthorizer', {
+      handler: authLambda,
+    });
+
+    const login = api.root.addResource('login');
+    login.addMethod('POST');
+
+    const complete = api.root.addResource('complete');
+    complete.addMethod('POST');
+
+    const loginLambda = new NodejsFunction(this, 'LoginLambda');
+    const completeLambda = new NodejsFunction(this, 'CompleteLambda');
+  }
+}
